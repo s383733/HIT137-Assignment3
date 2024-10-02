72d9340e5dca51592f374227825818e8db236658
s383733 <s383733@students.cdu.edu.au>
2024-10-02 15:50:38 +1000

    Add login screen

--- Q1.py
+++ Q1.py
@@ -1,12 +1,71 @@
 #Create a Tkinter application using the concepts of object-oriented programming, such as, multiple inheritance, multiple decorators, encapsulation, polymorphism, and method overriding, etc.
 
 import tkinter as tk        #Importing tkinter which will be used to create the GUI application
-from tkinter import filedialog  #Import filedialog which will be used for creating file selection windows
+from tkinter import filedialog
+from tkinter import messagebox  #Import filedialog which will be used for creating file selection windows
 from PIL import Image, ImageTk  #Import PIL for image handling within GUI
 from ultralytics import YOLO #Importing our simple AI model that will be incorporated in the application
 
+class Login_to_App:
+    def __init__(self, root):    
+        self.root = root    #Initialise a root window for the login screen
+        self.root.title("Sign in or Proceed as Guest")
+
+        self.title_label = tk.Label(root, text = "Welcome! Please choose an option:")   #Create a title for login screen
+        self.title_label.grid(row=0, column=0, pady=20)
+
+        self.login_button = tk.Button(root, text = "Sign in and search", command = self.login_page)
+        self.login_button.grid(row=1, column=0, padx=10, pady=10)
+
+        self.guest_button = tk.Button(root, text = "Search as Guest", command = self.guest_mode)
+        self.guest_button.grid(row=1, column=1, padx=10, pady=10)
+
+        self.or_label = tk.Label(root, text = "-------- or ---------")
+        self.or_label.grid(row=2, column=0, padx=20)
+
+        self.sign_up_button = tk.Button(root, text = "Create Account", command = self.create_account_page)
+        self.sign_up_button.grid(row=3, column=0, padx=20)
+    
+    def login_page(self):
+        self.login_screen = tk.Toplevel(self.root)
+        self.login_screen.title("Sign in")
+
+        self.username_label = tk.Label(self.login_screen, text = "Username")
+        self.username_label.grid(row=0, column=0, padx=5, pady=5)
+        self.password_label = tk.Label(self.login_screen, text = "Password")
+        self.password_label.grid(row=1, column=0, padx=5, pady=5)
+
+        self.username_entry = tk.Entry(self.login_screen)
+        self.username_entry.grid(row=0, column=0, padx=5, pady=5)
+        self.password_entry = tk.Entry(self.login_screen, show = "*")
+        self.password_entry.grid(row=1, column=0, padx=5, pady=5)
+
+        self.sign_in_button = tk.Button(self.login_screen, text = "Sign In", command = self.check_login_details)
+        self.sign_in_button.grid(row=2, column=0, columnspan=2, padx=5, pady=5)
+
+    def check_login_details(self):
+        username = self.username_entry.get()
+        password = self.password_entry.get()
+
+        if username == "HIT137" and password == "Group100":
+            messagebox.showinfo("Sign in successful")
+            self.login_screen.destroy()
+            self.open_AI_app(full_version = True)
+        else:
+            messagebox.showerror("Incorrect username or password")
+            self.login_screen.destroy()
+
+    def open_AI_app(self, full_version):
+        self.root.withdraw()
+        ai_window = tk.Toplevel(self.root)
+        app = AI_app(ai_window, full_version)
+
+    def guest_mode(self):
+        self.open_AI_app(full_version = False)
+
+
 class AI_app:
-    def __init__(self, root):
+    def __init__(self, root, full_version):
         self.root = root        #Initialise root window
         self.root.title("Animal Detection Service")     #Give a title to the window
 
@@ -31,29 +90,34 @@ class AI_app:
         self.upload_button = tk.Button(root, text = "Upload", command = self.upload_image)  #Create a button for image uploading
         self.upload_button.grid(row=1, column=2, padx=5, pady=5)      #Pack the button within the window
 
-        self.predict_label = tk.Label(root, text = "Predict the Animals")      #Create a label for image uploading
-        self.predict_label.grid(row=2, column=1, padx=5, pady=5)       #Pack the label into the window
-
-        self.predict_button = tk.Button(root, text = "Predict", command = self.detect_animal)  #Create a button to initiate animal prediciton
-        self.predict_button.grid(row=2, column=2, padx=5, pady=5)      #Pack the button within the window
-
+        if full_version:
+            self.predict_label = tk.Label(root, text = "Predict the Animals")      #Create a label for image uploading
+            self.predict_label.grid(row=2, column=1, padx=5, pady=5)       #Pack the label into the window
+            
+            self.predict_button = tk.Button(root, text = "Predict", command = self.detect_animal)  #Create a button to initiate animal prediciton
+            self.predict_button.grid(row=2, column=2, padx=5, pady=5)      #Pack the button within the window
+        else:
+            self.guest_label = tk.Label(root, text = "Limited Access: No Prediction Available, please sign up for predictions")
+            self.guest_label.grid(row=3, column=1, padx=5, pady=5)
+        
         self.result_label = tk.Label(root, text = "")   #Create a label for the results
         self.result_label.grid(row=3, column=1, padx=5, pady=5)    #Pack label within the window
 
-        self.model = YOLO('yolov8x.pt')
+        if full_version:
+            self.model = YOLO('yolov8x.pt')
 
     def upload_image(self):     #Create a method to upload images into the GUI
-        file_path = filedialog.askopenfilename()    #Create an Open dialog and return opened file object
-        self.image = Image.open(file_path)      
-        self.image_tk = ImageTk.PhotoImage(self.image)
-        self.image_label = tk.Label(image = self.image_tk)
+        file_path = filedialog.askopenfilename()    #Create an Open dialog and return opened file image
+        self.image = Image.open(file_path)      #Open the image using PIL
+        self.image_tk = ImageTk.PhotoImage(self.image)      #Converts the image to Tkinter format
+        self.image_label = tk.Label(image = self.image_tk)      #Displays the image in the GUI
         self.image_label.pack()     #Pack the label into the window
 
     def detect_animal(self):    #Create a method to detect the animal(s) in the image
-        prediction = self.model.predict(self.image)
-        for result in prediction:
-            print(result.boxes)
-            result.show()
+        prediction = self.model.predict(self.image)     #Assign model prediction to "prediction"
+        for result in prediction:   
+            print(result.boxes)     #print the boxes onto the image showing the classified animals
+            result.show()       #Show the results and confidence for each box
 
 root = tk.Tk()
 app = AI_app(root)
